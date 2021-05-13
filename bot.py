6c48f6d3e9515b2eeccf5e2431dc5d737e5b4dd4
Piotr Skrobski <72970475+Piotr-Skrobski@users.noreply.github.com>
2021-05-13 13:39:35 +0200

    Added auto-removing files.

--- bot.py
+++ bot.py
@@ -1,17 +1,20 @@
-# bot.py
 import os
-import random
-import discord
 from dotenv import load_dotenv
 import logging
-import os, sys, subprocess, shlex, re
+import os, subprocess
 from subprocess import call
-
-# 1
 from discord.ext import commands
+import discord
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 # 2
 bot = commands.Bot(command_prefix='!')
@@ -29,18 +32,29 @@ async def on_message(Message):
         if any(attachment.filename.lower().endswith(image) for image in image_types):
             await attachment.save(attachment.filename)
             if (probe_file(attachment.filename) == True):
+            	filenam = attachment.filename
             	print("NIELEGALNY PLIK")
             	await Message.delete()
             	channel = Message.channel
-            	await channel.send("Przepraszamy, plik, który wrzuciłeś/aś, był prawdopodobnie niebezpieczny i został automatycznie usunięty.")
+            	await channel.send("Przepraszamy, plik, który został wrzucony, był prawdopodobnie niebezpieczny i został automatycznie usunięty.")
+            	os.remove(filenam) 
             else:
             	print("Bezpieczny plik")
     
 #3
 
 def probe_file(filename):
-    cmnd = ['ffprobe', '-v', 'error', '-show_entries', 'frame=width,height', '-select_streams', 'v', '-of', 'csv=p=0', filename]
-    p = subprocess.Popen(cmnd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+    #Check for change in color space
+    cmd = ['ffmpeg', '-i', filename, '-vframes', '1', '-q:v', '1', 'first.jpg']
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    print(p.stdout.read())
+
+    #Check for change in resolution
+    cmd = ['ffprobe', '-v', 'error', '-show_entries', 'frame=width,height', '-select_streams', 'v', '-of', 'csv=p=0', filename]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     print(filename)
 
     lines = []
@@ -59,6 +73,5 @@ def probe_file(filename):
     
     return False
 
-#==========================================================================
 bot.run(TOKEN)
 
