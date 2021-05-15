0a85631884ea83abea2db9469db26c8aeeeff5e5
Piotr Skrobski <72970475+Piotr-Skrobski@users.noreply.github.com>
2021-05-15 16:28:19 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,6 @@ import discord
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
-
 # 2
 bot = commands.Bot(command_prefix='!')
 
